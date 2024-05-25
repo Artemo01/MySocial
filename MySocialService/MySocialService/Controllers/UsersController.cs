512a7be75d50babc6291b91541258c4da53dbcda
DESKTOP-JKUCR97\Artem <budzinski212@o2.pl>
2024-05-25 21:12:45 +0200

    feat(users): add first controller

---
+++ MySocialService/MySocialService/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MySocialService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        public UsersController(UserManager<IdentityUser> userManager) 
+        {
+            this.userManager = userManager;
+        }
+
+        //ONLY FOR TEST. LATER IT WILL BE REFACTOR INTO MVC
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserData()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            var user = await this.userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userData = new
+            {
+                user.UserName,
+                user.Email,
+                user.PhoneNumber,
+            };
+
+            return Ok(userData);
+        }
+    }
+}
