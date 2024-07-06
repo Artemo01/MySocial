83006b956e0169f40fdc519be5d91605e48b2ed0
DESKTOP-JKUCR97\Artem <budzinski212@o2.pl>
2024-07-06 20:58:36 +0200

    feat(profile): add profile template WIP

--- my-social/src/app/profile/profile.component.ts
+++ my-social/src/app/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
   public title = 'Profile';
